7053650a5bf6d39986ca0ed1bc1b8d4a318ff203
Daniel Thompson <DanThomp507@gmail.com>
2021-04-13 12:06:26 +0100

    fix: created modifier to check if owner or allowed

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -5,9 +5,27 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 pragma solidity >=0.7.4;
 
 contract SmipleWallet is Ownable {
+    mapping(address => uint256) public allowance;
+
+    function isOwner() private view returns (bool) {
+        return owner() == msg.sender;
+    }
+
+    function addAllowance(address _who, uint256 _amount) public onlyOwner {
+        allowance[_who] = _amount;
+    }
+
+    modifier ownerOrAllowed(uint256 _amount) {
+        require(
+            isOwner() || allowance[msg.sender] >= _amount,
+            "You are not allowed"
+        );
+        _;
+    }
+
     function withdrawMoney(address payable _to, uint256 _amount)
         public
-        onlyOwner
+        ownerOrAllowed(_amount)
     {
         _to.transfer(_amount);
     }
