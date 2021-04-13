6f09dfedd059f716909f3cc21fdabfc94e83d569
Daniel Thompson <DanThomp507@gmail.com>
2021-04-13 12:34:43 +0100

    feat: added wallet events

--- contracts/SharedWallet.sol
+++ contracts/SharedWallet.sol
@@ -6,6 +6,9 @@ import "./Allowance.sol";
 pragma solidity >=0.7.4;
 
 contract SharedWallet is Allowance {
+    event MoneySent(address indexed _beneficiary, uint256 _amount);
+    event MoneyReceived(address indexed _from, uint256 _amount);
+
     function withdrawMoney(address payable _to, uint256 _amount)
         public
         ownerOrAllowed(_amount)
@@ -17,8 +20,11 @@ contract SharedWallet is Allowance {
         if (!isOwner()) {
             reduceAllowance(msg.sender, _amount);
         }
+        emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
 
-    receive() external payable {}
+    receive() external payable {
+        emit MoneyReceived(msg.sender, msg.value);
+    }
 }
