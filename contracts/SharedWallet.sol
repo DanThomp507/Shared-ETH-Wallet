b5b48c3ae833153c4b92f8398a2cc5731e792d39
Daniel Thompson <DanThomp507@gmail.com>
2021-04-13 14:00:18 +0100

    fix: added renounce ownership function

--- contracts/SharedWallet.sol
+++ contracts/SharedWallet.sol
@@ -23,6 +23,10 @@ contract SharedWallet is Allowance {
         _to.transfer(_amount);
     }
 
+    function renounceOwnership() public override onlyOwner {
+        revert("can't renounceOwnership here");
+    }
+
     receive() external payable {
         emit MoneyReceived(msg.sender, msg.value);
     }
