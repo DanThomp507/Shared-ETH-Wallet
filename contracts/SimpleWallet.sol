d4ac93eb970fd7bc6edea836dfa71661c4bc33d1
Daniel Thompson <DanThomp507@gmail.com>
2021-04-13 11:54:12 +0100

    fix: imported Ownable from open zeppelin

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -1,19 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 pragma solidity >=0.7.4;
 
-contract SmipleWallet {
-    address public owner;
-
-    constructor() {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        require(owner == msg.sender, "You are not allowed");
-        _;
-    }
-
+contract SmipleWallet is Ownable {
     function withdrawMoney(address payable _to, uint256 _amount)
         public
         onlyOwner
